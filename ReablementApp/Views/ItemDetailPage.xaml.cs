218808b344952ce825825e1ce28023bd65bdd1ea
Colin Swan <Colinswan.cs@hotmail.co.uk>
2021-03-01 18:23:13 +0000

    Add project files.

---
+++ ReablementApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ReablementApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ReablementApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
