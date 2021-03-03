d10458b4fe0c917b0f4f65559adc6d45c49ea073
Colin Swan <Colinswan.cs@hotmail.co.uk>
2021-03-03 15:25:00 +0000

    Login Page started

--- ReablementApp/Views/LoginPage.xaml.cs
+++ ReablementApp/Views/LoginPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,43 @@ namespace ReablementApp.Views
         public LoginPage()
         {
             InitializeComponent();
+
+            SetPreferences();
+        }
+
+        private void SetPreferences()
+        {
+            LoginSwitch.IsToggled = Preferences.Get("loginSwitch", false);
+            if (!LoginSwitch.IsToggled)
+            {
+
+                Preferences.Clear();
+            }
+            else
+            {
+                EntPassword.Text = Preferences.Get("password", string.Empty);
+                EntUserName.Text = Preferences.Get("username", string.Empty);
+                LoginSwitch.IsToggled = Preferences.Get("loginSwitch", false);
+            }
+
+        }
+
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Set("username", EntUserName.Text);
+
+            //TODO stored access token
+            //var response = Preferences.Get("username", string.Empty);
+
+            Preferences.Set("password", EntPassword.Text);
+
+            //TODO stored access token
+            //var Passwordresponse = Preferences.Get("password", string.Empty);
+
+
+            Preferences.Set("loginSwitch", LoginSwitch.IsToggled);
+            var switchResponse = Preferences.Get("switch", false);
+
         }
     }
 }
