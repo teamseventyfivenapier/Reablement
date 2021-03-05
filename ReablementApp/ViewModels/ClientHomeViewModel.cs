d3b70ae1438dc88e2a02383b4780999825cadc2b
DARREN BRIDGES <darrenbridges@hotmail.co.uk>
2021-03-05 22:25:42 +0000

    Updated client homepage

--- ReablementApp/ViewModels/ClientHomeViewModel.cs
+++ ReablementApp/ViewModels/ClientHomeViewModel.cs
@@ -2,10 +2,27 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace ReablementApp.ViewModels
 {
     public class ClientHomeViewModel : BaseViewModel
     {
+        //public ObservableRangeCollection<Picture> Picture { get; set; }
+
+        public ClientHomeViewModel()
+        {
+            //Picture = new ObservableRangeCollection<Picture>();
+            //var image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min.png";
+
+            //Picture.Add(new Picture { Image = image });
+
+            // animate to 75% progress over 500 milliseconds with linear easing
+            // await progressBar.ProgressTo(0.75, 500, Easing.Linear);
+        }
+
+        public void GetUser()
+        {
+        }
     }
 }
