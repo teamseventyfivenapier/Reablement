78279dfdb28d65273de842dbb4f3339f646244eb
DARREN BRIDGES <darrenbridges@hotmail.co.uk>
2021-03-07 00:11:27 +0000

    Update Client home page and added Picture class.

---
+++ ReablementApp/Models/Picture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReablementApp.Models
+{
+    public class Picture
+    {
+        public string Quote { get; set; }
+        public string Image { get; set; }
+    }
+}
--- ReablementApp/ViewModels/ClientHomeViewModel.cs
+++ ReablementApp/ViewModels/ClientHomeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MvvmHelpers;
+using ReablementApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,14 +9,15 @@ namespace ReablementApp.ViewModels
 {
     public class ClientHomeViewModel : BaseViewModel
     {
-        //public ObservableRangeCollection<Picture> Picture { get; set; }
+        public ObservableRangeCollection<Picture> Picture { get; set; }
 
         public ClientHomeViewModel()
         {
-            //Picture = new ObservableRangeCollection<Picture>();
-            //var image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min.png";
+            Picture = new ObservableRangeCollection<Picture>();
+            var image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min.png";
 
-            //Picture.Add(new Picture { Image = image });
+            Picture.Add(new Picture { Quote = "Quote 1", Image = image });
+            Picture.Add(new Picture { Quote = "Quote 1", Image = image });
 
             // animate to 75% progress over 500 milliseconds with linear easing
             // await progressBar.ProgressTo(0.75, 500, Easing.Linear);
